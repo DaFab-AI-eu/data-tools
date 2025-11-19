16215a82369f1975ea8c6b505d833390fa323747
Metin Cakircali <metin.cakircali@ecmwf.int>
2025-11-19 13:30:50 +0000

    dump product ids into json

--- copernicus/search.py
+++ copernicus/search.py
@@ -1,12 +1,14 @@
 """
-Search Copernicus STAC for items based on user-defined parameters.
+Search Copernicus STAC for products based on user-defined parameters.
 
 Example usage:
-    Run this script with appropriate command line arguments to search the Copernicus STAC catalog and
-    print the IDs of the retrieved items.
+    python search.py --max_items 100 --collections sentinel-2-l2a --bbox "6.95,50.65,7.25,50.85" --datetime "2023-01-01/2023-12-31" --cloud_cover_max 20.0
 
 """
 
+import json
+
+from tifffile import product
 from helpers import get_seach_params
 from pystac_client import Client
 
@@ -20,15 +22,21 @@ __email__ = "metin.cakircali@ecmwf.int"
 def main():
     """Search Copernicus STAC and print found item IDs."""
 
-    catalog = Client.open("https://stac.dataspace.copernicus.eu/v1")
-    catalog.add_conforms_to("ITEM_SEARCH")
+    products = []
 
     params = get_seach_params()
 
-    results = catalog.search(**params)
+    catalog = Client.open("https://stac.dataspace.copernicus.eu/v1")
+    catalog.add_conforms_to("ITEM_SEARCH")
 
-    for item in results.items():
-        print(item.id)
+    resp = catalog.search(**params)
+
+    for item in resp.items():
+        products.append(item.id)
+
+    # Save product IDs to a JSON file
+    with open("/tmp/product_ids.json", "w") as f:
+        json.dump(products, f, indent=2)
 
 
 if __name__ == "__main__":
