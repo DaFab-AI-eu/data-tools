7107ffd320ae98cc9139406d0c6dafde3fe9598e
Joshua Harwood <joshua.harwood@ecmwf.int>
2026-06-18 14:48:44 +0200

    align log verbosity with depedencies

--- copernicus/logging_setup.py
+++ copernicus/logging_setup.py
@@ -5,26 +5,15 @@ def setup_logging(verbose: bool) -> None:
     """
     Configure root logging for the copernicus command-line tools.
     """
-    level = logging.DEBUG if verbose else logging.INFO
-
-    fmt = logging.Formatter
-    formatter = fmt("[%(name)-15s] %(levelname)-8s: %(message)s") if verbose else fmt("%(message)s")
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
+    log_format = "[%(name)-15s] %(levelname)-8s: %(message)s" if verbose else "%(message)s"
 
     console = logging.StreamHandler()
-    console.setFormatter(formatter)
+    console.setFormatter(logging.Formatter(log_format))
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG if verbose else logging.INFO)
     root_logger.handlers = [console]
 
-    # reduce chatter from dependencies
-    logging.getLogger("pydasi").setLevel(logging.WARNING)
-    logging.getLogger("utils").setLevel(logging.WARNING)
-    logging.getLogger("boto3").setLevel(logging.WARNING)
-    logging.getLogger("botocore").setLevel(logging.WARNING)
-
-    if verbose:
-        logging.getLogger("pystac").setLevel(logging.DEBUG)
-        logging.getLogger("pystac_client").setLevel(logging.DEBUG)
-        logging.getLogger("requests").setLevel(logging.DEBUG)
-        logging.getLogger("urllib3").setLevel(logging.DEBUG)
+    if not verbose:
+        for name in ("utils", "boto3", "botocore", "pystac", "pystac_client", "requests", "urllib3"):
+            logging.getLogger(name).setLevel(logging.WARNING)
